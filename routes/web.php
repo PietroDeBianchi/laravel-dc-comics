91f26382e0b5d5b862d0d63ad27fe57747daaedb
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-16 17:00:25 +0200

    back e front tested! now implementation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\ComicPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicPageController::class, 'index']);
