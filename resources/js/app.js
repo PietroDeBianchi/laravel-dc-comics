dfcd4ed21677623a18a240e86e5e32d8c70f3277
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-17 21:24:50 +0200

    try some extra

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,10 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+// });
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -24,7 +24,26 @@
                     <form action="{{route('comics.destroy', ['comic'=> $comic->id])}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this?')">Destroy!</button>                    </form>
+                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this?')">Destroy!</button>
+                        {{-- <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Destroy!</button> --}
+                        {{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </form>
                 </td>
             </tr>
         @endforeach
